77c8e8446314375978b77f6d5e6cc993837139da
mmari7 <mary.love.markova08@mail.ru>
2024-04-28 10:35:45 +0300

    registration complited

--- main.py
+++ main.py
@@ -1,17 +1,48 @@
-# This is a sample Python script.
+from flask import Flask, render_template, redirect
+from data import db_session
+from data.users import User
+from forms.user import RegisterForm
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')
-    print('dsfghj')# Press ⌘F8 to toggle the breakpoint.
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
 
-# Press the green button in the gutter to run the script.
+@app.route("/")
+def index():
+    db_sess = db_session.create_session()
+    # news = db_sess.query(News).filter(News.is_private == False)
+    return render_template("index.html", news=news)
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        if form.password.data != form.password_again.data:
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Пароли не совпадают")
+        db_sess = db_session.create_session()
+        if db_sess.query(User).filter(User.email == form.email.data).first():
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Такой пользователь уже есть")
+        user = User(
+            name=form.name.data,
+            email=form.email.data,
+            about=form.about.data
+        )
+        user.set_password(form.password.data)
+        db_sess.add(user)
+        db_sess.commit()
+        return redirect('/login')
+    return render_template('register.html', title='Регистрация', form=form)
+
+
+def main():
+    db_session.global_init("db/app.db")
+    app.run(port=1234)
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    main()
